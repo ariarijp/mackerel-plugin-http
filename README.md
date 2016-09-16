c002f89d861d8ae1d2314705017ea89639d50bd2
ariarijp <takuya.arita@gmail.com>
2016-09-16 21:23:22 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+mackerel-plugin-http
+=====================
+
+## Usage
+
+```shell
+$ mackerel-plugin-http -url "http://example.com"
+```
+
+## Example of mackerel-agent.conf
+
+```
+[plugin.metrics.http]
+command = "/path/to/mackerel-plugin-http -url \"http://example.com\""
+```
+
+## Author
+
+[Takuya Arita](https://github.com/ariarijp)
---
+++ http.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"time"
+
+	"github.com/ddliu/go-httpclient"
+	mp "github.com/mackerelio/go-mackerel-plugin-helper"
+)
+
+type HttpPlugin struct {
+	URL string
+}
+
+func (h HttpPlugin) FetchMetrics() (map[string]interface{}, error) {
+	stat := make(map[string]interface{})
+
+	func() {
+		defer func(start time.Time) {
+			stat["msec"] = uint64(time.Since(start) / time.Millisecond)
+		}(time.Now())
+
+		_, err := httpclient.Get(h.URL, map[string]string{})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	return stat, nil
+}
+
+func (h HttpPlugin) GraphDefinition() map[string](mp.Graphs) {
+	return map[string](mp.Graphs){
+		"http.response_time": mp.Graphs{
+			Label: "HTTP Response",
+			Unit:  "int",
+			Metrics: []mp.Metrics{
+				mp.Metrics{
+					Name:  "msec",
+					Label: "Milliseconds",
+				},
+			},
+		},
+	}
+}
+
+func main() {
+	optUrl := flag.String("url", "http://localhost/", "URL")
+	optTempfile := flag.String("tempfile", "/tmp/mackerel-plugin-http", "Temp file name")
+	flag.Parse()
+
+	var h HttpPlugin
+	h.URL = *optUrl
+
+	helper := mp.NewMackerelPlugin(h)
+	helper.Tempfile = *optTempfile
+
+	helper.Run()
+}
